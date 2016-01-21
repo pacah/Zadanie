dc704f5f3ea6e3a7965790640cff8908946273ae
parenb <mhaliullin@gmail.com>
2016-01-22 01:51:28 +0300

    bug

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Diary
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.i18n.default_locale = 'ru'
+    config.assets.initialize_on_precompile = false
   end
 end
